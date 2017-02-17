9e8acbba0df8976c693a90b34a11c6a8726a4879
zhengwei931102 <admin@admindemac-mini.local>
2017-02-17 09:55:45 +0800

    Initial Commit

---
+++ StatusItem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StatusItem
+//
+//  Created by admin on 17/2/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ StatusItem/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  StatusItem
+//
+//  Created by admin on 17/2/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
